6676fed1a5a1fba40bf11c8e2ba35650c861cf87
Puritized <pecamoint@gmail.com>
2025-11-21 06:40:29 +0100

    EduBridge

--- frontend/src/components/NotificationBell.jsx
+++ frontend/src/components/NotificationBell.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FaBell } from 'react-icons/fa';
+import { FaBell } from 'react-icons/fa6';
 import axios from 'axios';
 
 export default function NotificationBell() {
