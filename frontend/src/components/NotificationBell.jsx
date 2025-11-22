7e22d68f05c97dbf99cd3eef10b2c0669232c0e7
Puritized <pecamoint@gmail.com>
2025-11-22 10:58:10 +0100

    EduTech

--- frontend/src/components/NotificationBell.jsx
+++ frontend/src/components/NotificationBell.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FaBell } from 'react-icons/fa6';
 import axios from 'axios';
-import socket from '../utils/socket'; // make sure this points to your socket.js
+import socket from '../src/socket'; // make sure this points to your socket.js
 import { useAuth } from '../contexts/AuthContext';
 
 export default function NotificationBell() {
